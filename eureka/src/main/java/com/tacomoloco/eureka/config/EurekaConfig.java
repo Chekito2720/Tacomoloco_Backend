bdbecc82dadf5cc3eea822a09b63f53854bc1766
Chekito2720 <2023371075@uteq.edu.mx>
2026-07-08 20:07:22 -0600

    Configuración Eureka server

--- eureka/src/main/java/com/tacomoloco/eureka/config/EurekaConfig.java
+++ eureka/src/main/java/com/tacomoloco/eureka/config/EurekaConfig.java
@@ -1,1 +1,13 @@
 package com.tacomoloco.eureka.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EurekaConfig {
+
+    @Bean
+    public String eurekaInfo() {
+        return "Eureka Server running on port 8761";
+    }
+}
